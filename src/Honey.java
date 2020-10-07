d3b8edeb725aacbe6c77147bd363c8b78f582571
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-10-07 09:30:48 +0900

    Update, code format.

--- src/Honey.java
+++ src/Honey.java
@@ -18,33 +18,39 @@ class UserBean {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
 
-    public int getAge(){
+    public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
 
-    public String getSex(){
+    public String getSex() {
         return sex;
     }
+
     public void setSex(String sex) {
         this.sex = sex;
     }
 
-    public Date getBirthday(){
+    public Date getBirthday() {
         return birthday;
     }
+
     public void setBirthday(Date birthday) {
         this.birthday = birthday;
     }
-    public String getAddress(){
+
+    public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
